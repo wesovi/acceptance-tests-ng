f9e24ac3e2f9d81b05761f3d8a3032d55088fea5
Iván Corrales Solera <ivan.corrales.solera@gmail.com>
2015-04-09 04:15:27 +0200

    feat:initial commit

---
+++ src/main/java/com/wesovi/blog/DockerizingMicroServicesApplication.java
@@ -0,0 +1,12 @@
+package com.wesovi.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerizingMicroServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerizingMicroServicesApplication.class, args);
+    }
+}
---
+++ src/test/java/com/wesovi/blog/DockerizingMicroServicesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wesovi.blog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DockerizingMicroServicesApplication.class)
+public class DockerizingMicroServicesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
